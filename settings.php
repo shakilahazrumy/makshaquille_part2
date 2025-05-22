23321bf17d857553ac2854adc074349d9b243bb8
Denura-Git <105562893@student.swin.edu.au>
2025-05-23 01:23:00 +1000

    step 1

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost"; // The hostname of the database server, typically 'localhost' for local development.
-$user = "admin"; // The username used to connect to the database. 'root' is a common default user for local development.
-$pwd = "123456"; // The password associated with the database user. In this case, it's an empty string, indicating no password. This is common for local development but should be secured in production environments.
-$database = ""; // The name of the database to be used.
+$user = "root"; // The username used to connect to the database. 'root' is a common default user for local development.
+$pwd = ""; // The password associated with the database user. In this case, it's an empty string, indicating no password. This is common for local development but should be secured in production environments.
+$database = "eoi_db"; // The name of the database to be used.
 ?>
